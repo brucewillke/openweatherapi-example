4e53d4e03e00b1a50b89271972da986900f199b8
brucewillke <brucewillke@users.noreply.github.com>
2019-07-10 20:40:42 -0700

    Update openweatherapi.rb

--- ruby/openweatherapi.rb
+++ ruby/openweatherapi.rb
@@ -2,15 +2,16 @@
 
 require 'httparty'
 
-api_key     = "21c6977742eee43446ec20d18c0b3ffa"
-city        = "Los Angeles"
+api_key     = "21c6977742eee43446ec20d18c0b3ffa" # this api key would ideally be passed at runtime, but it's not sensitive in this case
+city        = "Los Angeles" # this could be an arg passed at run time as well, but this example only called for one city
 units       = "imperial"
  
 url         = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key + "&units=" + units
 
 response    = HTTParty.get(url)
 
-temperature = response['main']['temp'].ceil.to_s
+#using .ceil method here to round off the float to the nearest whole number
+temperature = response['main']['temp'].ceil.to_s 
 humidity    = response['main']['humidity'].ceil.to_s
 description = response['weather'][0]['description']
 
